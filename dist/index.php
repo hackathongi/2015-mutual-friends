c22279f1a1a0f9b9271bcacb6fe7efaed4b7c1f8
Jordi Corominas <jcorominas@ingeniummobile.com>
2015-03-28 20:44:31 +0100

    sending invitation with link

--- dist/index.php
+++ dist/index.php
@@ -29,34 +29,47 @@
         $friends = json_decode($result, true);
     }
     
-/*
-    $id_candidate, $id_job, $id_application;
     
-    http://recommendme.wallyjobs.com?$id_candidate=
-*/
     
+    // Retrieve application from BBDD
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Hackajobs";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "SELECT * FROM tbl_application WHERE id = " .$_GET["id_application"];
+    $result = $conn->query($sql);
+
+    $application = null;
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $application = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+
+    $textMail = "Bones, estic buscant feina i m'agradaria que em recomanessis per aquesta posició.";
+    $link = "https://recommendme.wallyjobs.com?id_candidate=" . $_GET["id_candidate"] . "&id_job=" . $application["job_id"] . "&id_application=" . $application["id"];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title><?php echo $jobtitle . ' | WallyJobs'; ?></title>
-
-    <meta property="og:type" content="website">
-        <meta property="og:url" content="<?php echo $currentUrl; ?>">
-        <meta property="og:title" content="<?php echo $job['title']; ?>">
-        <meta property="og:description" content="<?php echo $job['description']; ?>">
-        <meta property="og:image" content="<?php echo $job['picture_url']; ?>">
-
-        <meta property="twitter:card" content="summary">
-        <meta property="twitter:url" content="<?php echo $currentUrl; ?>">
-        <meta property="twitter:title" content="<?php echo $job['title']; ?>">
-        <meta property="twitter:description" content="<?php echo $job['description']; ?>">
-        <meta property="twitter:image" content="<?php echo $job['picture_url']; ?>">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link type="text/css" href="css/hackajobs-stylesheet.css" rel="stylesheet">
+    
+        <title>Mutual Friends</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link type="text/css" href="css/hackajobs-stylesheet.css" rel="stylesheet">
     
     <script src="http://connect.facebook.net/en_US/all.js"></script>
 
@@ -67,8 +80,8 @@
 
       FB.ui({
           method: 'send',
-          name: 'People Argue Just to Win',
-          link: 'https://www.facebook.com/',
+          name: '<?=urlencode($textMail)?>',
+          link: '<?=$link?>',
           });
       };
      </script>
@@ -76,7 +89,6 @@
 <body>
 
 <header>
-
     <div class="container">
         <div class="wally-logo">
             <img src="img/wallyJobsLogoHor.svg" alt="WallyJobs"/>
@@ -86,40 +98,44 @@
 
 </header>
 
-<main>
-    <div class="container">
-        <article class="job-offer offer">
-            <h1 class="offer-title"><?php echo $jobtitle; ?></h1>
-            
-            <div class="friends">
+    <main class="container">
+
+        <article class="offer-status">
+
+            <h1 class="offer-title">Cambrer restaurant Platja</h1>
+
+            <section class="offer-mutual-friends">
+                <h4 class="mutual-friends-title">Tens <?=count($friends); ?> amics en comú amb l'anunciant:</h4>
+                <div class="friends">
                 <?php
                 foreach ($friends as &$friend) {
-                    // var_dump($friend);
-                    echo "<div>name:" . $friend["name"] . "</div>";
-                    echo "<img src=" . $friend["picture_url"] ." width=\"200px\", height=\"200px\" alt=\"Titol Oferta\"/>";
+                ?>
+                    <article class="mutual-friend">
+                        <div class="friend-image">
+                            <img src="<?=$friend["picture_url"]?>">
+                        </div>
+                        <h5 class="friend-name"><?=$friend["name"]?> </h5>
+                        <a class="friend-recommendation-request" href="#" onclick="fbClick();"><span class="badge">Sol·licita una recomanació</span></a>
+                    </article>
+                            
+                <?php
                 }
                 ?>
-            </div>
-            <hr/>
-            <div class="offer-apply">
-                <a class="btn btn-primary btn-lg" href="<?php echo $url_fb; ?>">Apply</a>
-            </div>
-
-            <div class="offer-apply">
-                <a class="btn btn-primary btn-lg" href="#" onclick="fbClick();">Demanar als amics</a>
-            </div>
+                </div>
+            </section>
         </article>
+    </main>
+
+
+    <div class="offer-apply">
+        <a class="btn btn-primary btn-lg" href="<?php echo $url_fb; ?>">Apply</a>
     </div>
 
-</main>
+    <footer>
+        <p class="small">©2015 Bla bla bla, ...</p>
+    </footer>
 
-<footer>
-    <p class="small">©2015 Bla bla bla, ...</p>
-</footer>
-
-
-
-<script type="text/javascript" src="js/hackajobs-stylesheet-dependencies.js"></script>
-
+    <script type="text/javascript" src="js/hackajobs-stylesheet-dependencies.js"></script>
+    
 </body>
 </html>
